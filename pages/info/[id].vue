c6413149757fcef87fe46842ef4e9b5dc472fd5c
sckoorp <ckamalennour@gmail.com>
2024-07-06 22:58:12 +0100

    update: added optional chaining

--- pages/info/[id].vue
+++ pages/info/[id].vue
@@ -12,67 +12,67 @@ const { data } = await useAsyncData("info", async () => {
 <template>
     <div class="grid grid-cols-1 lg:grid-cols-[auto,1fr] gap-8 m-4">
         <div class="hidden lg:flex flex-col gap-2">
-            <img :src="data.info.cover" :alt="data.info.title" class="w-56 h-80 rounded-sm object-cover">
+            <img :src="data?.info.cover" :alt="data?.info.title" class="w-56 h-80 rounded-sm object-cover">
         </div>
         <div class="lg:hidden flex flex-col items-center gap-2">
-            <img :src="data.info.cover" :alt="data.info.title"
+            <img :src="data?.info.cover" :alt="data?.info.title"
                 class="w-40 h-60 md:w-48 md:h-72 rounded-sm object-cover">
             <div class="flex flex-col justify-center items-center text-center">
-                <p class="text-light text-base font-normal">{{ data.info.season }} {{ data.info.year }}</p>
-                <p class="text-light text-2xl font-bold line-clamp-3">{{ data.info.title }}</p>
+                <p class="text-light text-base font-normal">{{ data?.info.season }} {{ data?.info.year }}</p>
+                <p class="text-light text-2xl font-bold line-clamp-3">{{ data?.info.title }}</p>
             </div>
         </div>
         <div class="flex flex-col gap-2">
             <div class="hidden lg:flex flex-col">
-                <p class="text-light text-base font-normal">{{ data.info.season }} {{ data.info.year }}</p>
-                <p class="text-light text-2xl font-bold line-clamp-2">{{ data.info.title }}</p>
+                <p class="text-light text-base font-normal">{{ data?.info.season }} {{ data?.info.year }}</p>
+                <p class="text-light text-2xl font-bold line-clamp-2">{{ data?.info.title }}</p>
             </div>
             <div class="flex flex-wrap items-center gap-2">
                 <button type="button" class="text-dark bg-prime outline-none rounded-sm px-2"
-                    v-for="genre in data.info.genres">{{ genre }}</button>
+                    v-for="genre in data?.info.genres">{{ genre }}</button>
             </div>
             <div class="bg-prime/5 space-y-2 rounded-sm p-4">
                 <p class="text-light text-lg font-semibold">Synopsis</p>
-                <div v-html="data.info.description" class="text-light/75 text-base font-normal" />
+                <div v-html="data?.info.description" class="text-light/75 text-base font-normal" />
             </div>
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-2">
                 <div class="space-y-2">
                     <div class="bg-prime/5 space-y-1 rounded-sm p-4">
                         <p class="text-light text-lg font-semibold">Format</p>
-                        <p class="text-light/75 text-base font-normal">{{ data.info.format }}</p>
+                        <p class="text-light/75 text-base font-normal">{{ data?.info.format }}</p>
                     </div>
                     <div class="bg-prime/5 space-y-1 rounded-sm p-4">
                         <p class="text-light text-lg font-semibold">Status</p>
-                        <p class="text-light/75 text-base font-normal">{{ data.info.status }}</p>
+                        <p class="text-light/75 text-base font-normal">{{ data?.info.status }}</p>
                     </div>
                 </div>
                 <div class="space-y-2">
                     <div class="bg-prime/5 space-y-1 rounded-sm p-4">
                         <p class="text-light text-lg font-semibold">Score</p>
-                        <p class="text-light/75 text-base font-normal" v-if="data.info.score">{{ data.info.score }}</p>
+                        <p class="text-light/75 text-base font-normal" v-if="data?.info.score">{{ data?.info.score }}</p>
                         <p class="text-light/75 text-base font-normal" v-else>N/A</p>
                     </div>
                     <div class="bg-prime/5 space-y-1 rounded-sm p-4">
                         <p class="text-light text-lg font-semibold">Episodes</p>
-                        <p class="text-light/75 text-base font-normal" v-if="data.info.episodes">
-                            {{ data.info.episodes }} Episode(s)</p>
+                        <p class="text-light/75 text-base font-normal" v-if="data?.info.episodes">
+                            {{ data?.info.episodes }} Episode(s)</p>
                         <p class="text-light/75 text-base font-normal" v-else>N/A</p>
                     </div>
                 </div>
             </div>
             <div class="bg-prime/5 space-y-1 rounded-sm p-4">
                 <p class="text-light text-lg font-semibold">Studio</p>
-                <p class="text-light/75 text-base font-normal" v-if="data.info.studio">{{ data.info.studio }}</p>
+                <p class="text-light/75 text-base font-normal" v-if="data?.info.studio">{{ data?.info.studio }}</p>
                 <p class="text-light/75 text-base font-normal" v-else>N/A</p>
             </div>
         </div>
     </div>
-    <div class="space-y-4 m-4" v-if="data.recommendations.recommendations.length > 0">
+    <div class="space-y-4 m-4" v-if="data?.recommendations.recommendations.length > 0">
         <div class="flex justify-between items-center">
             <p class="text-light text-xl font-bold uppercase">Recommendations</p>
         </div>
         <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-2">
-            <NuxtLink v-for="anime in data.recommendations.recommendations.slice(0, 8)" :to="'/info/' + anime.id"
+            <NuxtLink v-for="anime in data?.recommendations.recommendations.slice(0, 8)" :to="'/info/' + anime.id"
                 class="relative flex flex-col group gap-2">
                 <NuxtImg :src="anime.cover" :alt="anime.title" placeholder
                     class="w-full h-full rounded-sm object-cover" />
