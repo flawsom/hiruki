f7b64b8dfbf99f9bc3ed3a217991fc78ebd84d61
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-07-16 20:21:49 +0530

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,13 +6,13 @@
         <div class="flex flex-col md:flex-row justify-between md:items-center gap-4">
             <div>
                 <ULink to="/" class="text-4xl font-bold">
-                    Hiruki</ULink>
+                    Ani.MoM</ULink>
                 <p class="md:w-96">This site does not store any files on our server, we
                     only linked to the media which is hosted on 3rd party services.</p>
             </div>
             <div class="flex flex-col gap-2">
                 <UButton to="/dmca" icon="i-heroicons-book-open" label="DMCA" variant="ghost" />
-                <UButton to="https://github.com/sckoorp/hiruki/issues" target="_blank" icon="i-heroicons-bug-ant"
+                <UButton to="#" icon="i-heroicons-bug-ant"
                     label="Bug Report" variant="ghost" />
             </div>
         </div>
